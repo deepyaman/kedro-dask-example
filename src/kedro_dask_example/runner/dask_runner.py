3b1947e4f8100cd38986a6b7a228a43fe2d73917
Deepyaman Datta <deepyaman.datta@utexas.edu>
2021-12-24 21:00:29 -0500

    Implement customized `DaskRunner.run_only_missing`

--- src/kedro_dask_example/runner/dask_runner.py
+++ src/kedro_dask_example/runner/dask_runner.py
@@ -29,8 +29,7 @@ class _DaskDataSet(AbstractDataSet):
             client.publish_dataset(data, name=self._name, override=True)
 
     def _exists(self) -> bool:
-        with worker_client() as client:
-            return self._name in client.list_datasets()
+        return self._name in Client.current().list_datasets()
 
     def _release(self) -> None:
         Client.current().unpublish_dataset(self._name)
@@ -139,3 +138,53 @@ class DaskRunner(AbstractRunner):
                     and data_set not in pipeline.outputs()
                 ):
                     catalog.release(data_set)
+
+    def run_only_missing(
+        self, pipeline: Pipeline, catalog: DataCatalog
+    ) -> Dict[str, Any]:
+        """Run only the missing outputs from the ``Pipeline`` using the
+        datasets provided by ``catalog``, and save results back to the
+        same objects.
+
+        Args:
+            pipeline: The ``Pipeline`` to run.
+            catalog: The ``DataCatalog`` from which to fetch data.
+        Raises:
+            ValueError: Raised when ``Pipeline`` inputs cannot be
+                satisfied.
+
+        Returns:
+            Any node outputs that cannot be processed by the
+            ``DataCatalog``. These are returned in a dictionary, where
+            the keys are defined by the node outputs.
+        """
+        free_outputs = pipeline.outputs() - set(catalog.list())
+        missing = {ds for ds in catalog.list() if not catalog.exists(ds)}
+        to_build = free_outputs | missing
+        to_rerun = pipeline.only_nodes_with_outputs(*to_build) + pipeline.from_inputs(
+            *to_build
+        )
+
+        # We also need any missing datasets that are required to run the
+        # `to_rerun` pipeline, including any chains of missing datasets.
+        unregistered_ds = pipeline.data_sets() - set(catalog.list())
+        # Some of the unregistered datasets could have been published to
+        # the scheduler in a previous run, so we need not recreate them.
+        missing_unregistered_ds = {
+            ds_name for ds_name in unregistered_ds
+            if not self.create_default_data_set(ds_name).exists()
+        }
+        output_to_unregistered = pipeline.only_nodes_with_outputs(
+            *missing_unregistered_ds
+        )
+        input_from_unregistered = to_rerun.inputs() & missing_unregistered_ds
+        to_rerun += output_to_unregistered.to_outputs(*input_from_unregistered)
+
+        # We need to add any previously-published, unregistered datasets
+        # to the catalog passed to the `run` method, so that it does not
+        # think that the `to_rerun` pipeline's inputs are not satisfied.
+        catalog = catalog.shallow_copy()
+        for ds_name in unregistered_ds - missing_unregistered_ds:
+            catalog.add(ds_name, self.create_default_data_set(ds_name))
+
+        return self.run(to_rerun, catalog)
